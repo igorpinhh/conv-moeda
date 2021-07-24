9356d88b87d8ee56b6e706a98f5029d105d76f80
Igor Pinheiro <31784083+igorpinhh@users.noreply.github.com>
2021-07-24 06:17:04 -0300

    adding some features and new styles

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const url = "https://economia.awesomeapi.com.br/all/USD-BRL,EUR-BRL,BTC-BRL";
+const url = "https://economia.awesomeapi.com.br/all";
+
 
 const div = document.querySelector(".resultado");
 const btn = document.querySelector("#btn");
@@ -10,6 +11,7 @@ const reset = document.querySelector("#reset");
 const ajax = () => {
   const number = document.querySelector("input").value;
   const moeda = document.querySelector("#moeda");
+  const status = document.querySelector('#cotacao')
 
   fetch(url)
     .then((resp) => resp.json())
@@ -17,18 +19,57 @@ const ajax = () => {
       const api = data;
       let dolar = api.USD.high;
       let euro = api.EUR.high;
+      let cad = api.CAD.high;
+      let ltc = api.LTC.high;
+      let chf = api.CHF.high
+      let aud = api.AUD.high
+      // console.log(dolar);
 
       if (moeda.value === "dolar") {
-        let x = parseFloat(dolar, 2).toFixed(2);
-        let f = number * x;
-        div.innerHTML = '$ ' + f.toFixed(2)
+        let f = number / dolar;
+        div.innerHTML = '$ ' + f.toFixed(2);
+        // console.log();
+        status.innerHTML = '$ ' + dolar
+      } 
+
+      else if (moeda.value === "") {
+        status.innerHTML = '';
       } 
       
       else if (moeda.value === "euro") {
-        let x = parseFloat(euro, 2).toFixed(2);
-        let f = number * x;
+        let f = number / euro;
         div.innerHTML = '€ ' + f.toFixed(2);
-      } else {
+        status.innerHTML = '$ ' + euro;
+      } 
+
+      else if (moeda.value === "cad") {
+        let x = parseFloat(cad, 2).toFixed(2);
+        let f = number / x;
+        div.innerHTML = '€ ' + f.toFixed(2);
+        status.innerHTML = '$ ' + cad;
+      }
+
+      else if (moeda.value === "ltc") {
+        let f = number / ltc;
+        div.innerHTML = '€ ' + f.toFixed(2);
+        status.innerHTML = '$ ' + ltc;
+      }
+
+      else if (moeda.value === "aud") {
+        let x = parseFloat(aud, 2).toFixed(2);
+        let f = number / x;
+        div.innerHTML = '€ ' + f.toFixed(2);
+        status.innerHTML = '$ ' + aud;
+      }
+
+      else if (moeda.value === "chf") {
+        let x = parseFloat(chf, 2).toFixed(2);
+        let f = number / x;
+        div.innerHTML = '€ ' + f.toFixed(2);
+        status.innerHTML = '$ ' + chf;
+      }
+      
+      else {
         div.innerHTML = "deu erro ai vacilao";
     }
   });
